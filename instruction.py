2992506b625929375274438743b14ad4a2604845
Phillip Quiza <aznwhtey@gmail.com>
2012-03-04 23:08:02 -0500

    Added function to determine whether instruction reads from memory

--- instruction.py
+++ instruction.py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python
+latencies = {
+        'nop': 1, 
+        'addI': 1, 
+        'add': 1, 
+        'subI': 1, 
+        'sub': 1, 
+        'mult': 3, 
+        'div': 3, 
+        'load': 5, 
+        'loadI': 1, 
+        'loadAI': 5, 
+        'loadAO': 5, 
+        'store': 5, 
+        'storeAO': 5, 
+        'storeAI': 5, 
+        'output': 1
+        }
 
-latency = {'nop':1, 'addI':1, 'add':1, 'subI':1, 'sub':1, 'mult':3, 'div':3, 'load':5, 'loadI':1, 'loadAI':5, 'loadAO':5, 'store':5, 'storeAO':5, 'storeAI':5, 'output':1}
-
-class Instruction():
+class Instruction(object):
     """
     Represents Instructions, and gives information about certain instructions
     """
@@ -13,7 +27,7 @@ class Instruction():
         parts             = [x.strip(',') for x in instruction.split()]
         self.line         = line
         self.op           = parts[0]
-        self.latency      = latency[self.op]
+        self.latency      = latencies[self.op]
         self.deps         = {'true':set([]), 'anti':set([])}
         self.priority     = 0
         self.args         = []
@@ -49,3 +63,15 @@ class Instruction():
         if self.op in set(['store', 'storeAI', 'storeAO']):
             return True
         return False
+
+    def is_mem_read(self):
+        """
+        Returns True if instruction reads from memory, else returns False
+        """
+        if self.op in set(['load','loadAI','loadAO','output']):
+            return True
+        return False
+    
+    @staticmethod
+    def get_priority(instruction):
+        return instruction.priority
